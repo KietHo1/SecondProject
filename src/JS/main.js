0783874860ddbe9871cfa76f115ebb22c63443d2
KietHo1 <kiethostartup1999@gmail.com>
2022-10-06 21:57:18 +0700

    Edit

--- src/JS/main.js
+++ src/JS/main.js
@@ -64,7 +64,7 @@ let swiperProjects = new Swiper(".projects_container", {
         },
     },
 });
-
+window.addEventListener('swiper', swiperProjects)
 /*=============== SWIPER TESTIMONIAL ===============*/
 let swiperTestimonial = new Swiper(".testimonial_container",{
     grabCursor: true,
@@ -74,7 +74,7 @@ let swiperTestimonial = new Swiper(".testimonial_container",{
         prevEl: ".swiper-button-prev",
     },
 });
-
+window.addEventListener('testimonial', swiperTestimonial)
 /*======================= EMAIL JS ========================*/
 const contactForm = document.createElementById('contact-form'),
     contactName = document.createElementById('contact-name'),
@@ -117,3 +117,40 @@ const sendEmail = (e) => {
 }
 
 contactForm.addEventListener('submit', sendEmail);
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
