d5b7137f8c0ffa392f5085487d10bd4b7d5b5965
KietHo1 <kiethostartup1999@gmail.com>
2022-10-07 13:47:39 +0700

    Edit

--- src/JS/main.js
+++ src/JS/main.js
@@ -179,3 +179,12 @@ const scrollHeader = () =>{
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true /* Animations repeat */
+})
+
+sr.reveal('.home_data')
